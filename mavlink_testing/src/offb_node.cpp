02267abef00730e0aba835f8bc04ca35c8b96a6b
Madis <madis1995@gmail.com>
2018-09-25 20:36:10 +0300

    added launch files for mavros launch

--- mavlink_testing/src/offb_node.cpp
+++ mavlink_testing/src/offb_node.cpp
@@ -39,16 +39,16 @@ int main(int argc, char **argv)
     }
 
     geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 2;
+    pose.pose.position.x = 1;
+    pose.pose.position.y = 1;
+    pose.pose.position.z = 1.5;
 
-    //send a few setpoints before starting
-    for(int i = 100; ros::ok() && i > 0; --i){
-        local_pos_pub.publish(pose);
-        ros::spinOnce();
-        rate.sleep();
-    }
+    // //send a few setpoints before starting
+    // for(int i = 100; ros::ok() && i > 0; --i){
+    //     local_pos_pub.publish(pose);
+    //     ros::spinOnce();
+    //     rate.sleep();
+    // }
 
     mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "OFFBOARD";
