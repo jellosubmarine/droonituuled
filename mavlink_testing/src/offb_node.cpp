a83856bb129382541059ea158404b6a4e68cd835
Rando Avarmaa <avarmaarando@gmail.com>
2018-10-23 21:46:10 +0300

    APM almost working (simu bit broken)

--- mavlink_testing/src/offb_node.cpp
+++ mavlink_testing/src/offb_node.cpp
@@ -9,6 +9,7 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
+#include <mavros_msgs/OverrideRCIn.h>
 
 mavros_msgs::State current_state;
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
@@ -29,6 +30,9 @@ int main(int argc, char **argv)
     ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
             ("mavros/set_mode");
 
+    // RC publisher
+    ros::Publisher pub = nh.advertise<mavros_msgs::OverrideRCIn>("/mavros/rc/override", 10);
+
     //the setpoint publishing rate MUST be faster than 2Hz
     ros::Rate rate(30.0);
     ROS_INFO("Test");
@@ -39,33 +43,35 @@ int main(int argc, char **argv)
     }
     ROS_INFO("test2");
 
-    geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 1;
-    pose.pose.position.y = 1;
-    pose.pose.position.z = 1.5;
-
-    // //send a few setpoints before starting
-    // for(int i = 100; ros::ok() && i > 0; --i){
-    //     local_pos_pub.publish(pose);
-    //     ros::spinOnce();
-    //     rate.sleep();
-    // }
 
     mavros_msgs::SetMode offb_set_mode;
-    offb_set_mode.request.custom_mode = "OFFBOARD";
+    offb_set_mode.request.custom_mode = "MANUAL";
 
     mavros_msgs::CommandBool arm_cmd;
     arm_cmd.request.value = true;
 
     ros::Time last_request = ros::Time::now();
 
+    mavros_msgs::OverrideRCIn msg;
+
+    msg.channels[5] = 1600; // forward  (x)
+    msg.channels[6] = 1900; // strafe   (y)
+    msg.channels[2] = 1550; // throttle (z)
+
+    msg.channels[1] = 1900; // roll     (wx)
+    msg.channels[0] = 1900; // pitch    (wy)
+    msg.channels[3] = 1900; // yaw      (wz)
+
+    msg.channels[4] = 1900; // mode
+    msg.channels[7] = 1900; // camera tilt
+
     while(ros::ok()){
 
-        if( current_state.mode != "OFFBOARD" &&
+        if( current_state.mode != "MANUAL" &&
             (ros::Time::now() - last_request > ros::Duration(5.0))){
             if( set_mode_client.call(offb_set_mode) &&
                 offb_set_mode.response.mode_sent){
-                ROS_INFO("Offboard enabled");
+                ROS_INFO("Manual enabled");
             }
             last_request = ros::Time::now();
         } else {
@@ -79,7 +85,7 @@ int main(int argc, char **argv)
             }
         }
 
-        local_pos_pub.publish(pose);
+        pub.publish(msg);
 
         ros::spinOnce();
         rate.sleep();
