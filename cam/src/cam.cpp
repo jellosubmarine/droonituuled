b683f97bcfb437fd883576869453a520ad1272f6
Johan Kutt <johan.kytt@gmail.com>
2018-11-15 02:32:32 +0200

    Refactored cam code. Added optical flow from Rando. Doesn't publish velocities yet.

--- cam/src/cam.cpp
+++ cam/src/cam.cpp
@@ -1,20 +1,13 @@
-#include "dt_config.hpp"
-
-#include <cv_bridge/cv_bridge.h>
-#include <image_transport/image_transport.h>
-#include <ros/ros.h>
-#include <sensor_msgs/image_encodings.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <ctime>
+//#include <ctime>
 #include <iostream>
 #include <numeric>
-#include "geometry_msgs/QuaternionStamped.h"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/opencv.hpp"
+
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include "dt_config.hpp"
+#include "cam.hpp"
 
 using namespace std;
 using namespace cv;
@@ -27,263 +20,268 @@ using namespace cv;
 #endif
 
 
-class visuals
-{
-  protected:
-    ros::NodeHandle nh_;
-    image_transport::ImageTransport it_;
-    geometry_msgs::QuaternionStamped msg;
-    image_transport::Subscriber sub;
-    ros::Publisher point_pub;
+/* CONSTRUCTORS AND CALLBACKS */
 
-  private:
+Visuals::Visuals(ros::NodeHandle &nh_) : it_(nh_) {
+  image_flag = false;
+  sub = it_.subscribe("/camera/color/image_raw", 1, &Visuals::imgcb, this);
+  point_pub = nh_.advertise<geometry_msgs::QuaternionStamped>(DT_CAM_TOPIC, 1);
+}
 
-    // MOST OF NEEDED DEFINITIONS HERE
+#ifdef DT_BUILD_DEV
+  Visuals::~Visuals() { destroyWindow("Contours"); }
+#endif
 
-    bool image_flag;
+void Visuals::imgcb(const sensor_msgs::ImageConstPtr &msg) {
+  frame_time = ros::Time::now().toSec();
+  frame = cv_bridge::toCvCopy(msg, "bgr8")->image;
+  cvtColor(frame, frame_gray, CV_BGR2GRAY); // no risk of overwriting prematurely
+  image_flag = true;
+}
 
-    // IMPORTANT CHENAGEABLE PARAMETERS HERE
 
-    //const static bool visualize = false; // True for visuals, false to turn visualizing off
+/* IMAGE PROCESSING */
 
-    const static int width = 640;    // Frame width
-    const static int height = 480;   // Frame height
-    const static int lower  = 1000;  // Lower limit for contour size
-    const static int upper = 10000;  // Upper limit for contour size
-    const static int X_offset = -(width / 2); // X gets shifted into the middle of the frame .
-    const static int Y_offset = -(height / 2); // Y shift from the bottom of the frame.
-    const static int blur_size = 3; // Blurring size
-    const static int lowThreshold = 121; // Canny lower threshold
-    const static int ratio = 1.5;  // Canny lower Tresh * ratio = upper thresh
-    const static int kernel_size = 3; // Canny kernel size
-    const static int mask_w = 200; // Mask cutoff from sides
+void Visuals::process() {
+  #ifdef DT_BUILD_DEV
+    // Start time for frame rate calculation
+    float startTime = ros::Time::now().toSec(); //std::clock_t start( std::clock() );
+  #endif
 
-    Mat frame;
-    Mat img_gray;
-    Mat img_bw;
-    Mat erod;
-    Mat dil;
-    Mat masked;
-    Mat blurr;
-    Mat img_final;
-    Mat canny_output;
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    float sum_x, sum_y, X, Y, bot_X, bot_Y, Z;
-    int W;
 
-  public:
+  // Update optical flow tracked points at given interval
+  if (of_refresh_counter >= CAM_OF_REFRESH_INTERVAL) {
+    of_refresh_counter = 0;
+    of_old_points.clear();
 
-    visuals(ros::NodeHandle &nh_, ros::NodeHandle &private_) : it_(nh_)
-    {
-        image_flag = false;
-        sub = it_.subscribe("/camera/color/image_raw", 1, &visuals::imgcb, this);
-        point_pub = nh_.advertise<geometry_msgs::QuaternionStamped>(DT_CAM_TOPIC, 1);
+    // Detect keypoints and extract raw point data
+    feature_detector->detect(frame_gray_old, of_keypoints);
+    for (size_t i = 0; i < of_keypoints.size(); ++i){
+      of_old_points.push_back(of_keypoints[i].pt);
     }
+    // goodFeaturesToTrack(gray_old, of_old_points, maxCorners, qualityLevel, minDistance, noArray(), blockSize);
+  }
+
+
+  // Calculate optical flow
+  if (!of_old_points.empty()) {
+    calcOpticalFlowPyrLK( frame_gray_old, frame_gray,
+                          of_old_points, of_new_points,
+                          of_status, of_err, of_win_size,
+                          CAM_OF_MAX_LEVELS, of_criteria );
+  }
+
+  // select good points
+  //vector<Point2f> goodNew(st.size());
+  //vector<Point2f> goodOld(st.size());
+
+  // Calculate velocities
+  of_velocities.clear();
+  of_mean_velocity.x = 0.0;
+  of_mean_velocity.y = 0.0;
+
+  for (size_t i = 0; i < of_status.size(); ++i) {
+    if (of_status[i]) {
+      of_velocities.push_back( cv::Point2f(
+        (of_new_points[i].x - of_old_points[i].x) / (frame_time - frame_old_time) / float(CAM_FRAME_WIDTH) * 2.0f,  // Normalised to half frame
+        (of_old_points[i].y - of_new_points[i].y) / (frame_time - frame_old_time) / float(CAM_FRAME_HEIGHT) * 2.0f
+      ) );
+      of_mean_velocity.x += of_velocities[i].x;
+      of_mean_velocity.y += of_velocities[i].y;
+      //goodOld.push_back(of_old_points[i]);
+      //goodNew.push_back(of_new_points[i]);
+    }
+  }
+
+  of_mean_velocity.x /= float(of_velocities.size());
+  of_mean_velocity.y /= float(of_velocities.size());
+
+  // Finished with OF
+
+
+  // Process and find contours
+  blur(frame_gray, blurr, Size(CAM_BLUR_SIZE, CAM_BLUR_SIZE), Point(-1, -1));       // Blur image
+  Canny(blurr, dil, CAM_CANNY_THR_LOW, CAM_CANNY_THR_HIGH, CAM_CANNY_KERNEL_SIZE ); // Find edges
+  dilate(dil, erod, Mat(), Point(-1, -1), 4, 1, 1);                                 // Get rid of holes
+  //erode(dil, dil, Mat(), Point(-1, -1), 4, 1, 1);
+
+  erod.copyTo(canny_output, mask);
+  findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0)); // Get contours
+
+
+  // Clear storage vectors
+  mu.clear();
+  avg_x.clear();
+  avg_y.clear();
+  #ifdef DT_BUILD_DEV
+    mc.clear();
+  #endif
+
+  // Calculate contour moments
+  for (size_t i = 0; i < contours.size(); ++i) { // Look through all contours
+    if (contourArea(contours[i]) > CAM_CONTOUR_LIM_LOW &&
+        contourArea(contours[i]) < CAM_CONTOUR_LIM_HIGH)
+    {
+      mu.push_back(moments(contours[i], false));
+    }
+  }
+
+  // Calculate centroids from moments
+  for (size_t i = 0; i < mu.size(); i++) {
+    avg_x.push_back( float(mu[i].m10 / mu[i].m00) );
+    avg_y.push_back( float(mu[i].m01 / mu[i].m00) );
 
     #ifdef DT_BUILD_DEV
-      ~visuals() { destroyWindow("Contours"); }
+      // For visualisation
+      mc.push_back( Point2f( static_cast<float>(mu[i].m10 / mu[i].m00),
+                             static_cast<float>(mu[i].m01 / mu[i].m00) ) );
     #endif
 
-    void imgcb(const sensor_msgs::ImageConstPtr &msg)
-    {
-        frame = cv_bridge::toCvCopy(msg, "bgr8")->image;
-        image_flag = true;
+    if (i==0){
+        bot_X = static_cast<float>(mu[i].m10 / mu[i].m00);
+        bot_Y = static_cast<float>(mu[i].m01 / mu[i].m00);
     }
-    ///////////////////////////////////////////////////////////////////////////////////////////////
+  }
 
-    // MAIN PROCESS STARTS HERE
+  // Calculate mean centroid
+  sum_x = accumulate(avg_x.begin(), avg_x.end(), 0); // Sum all centroids' x values
+  sum_y = accumulate(avg_y.begin(), avg_y.end(), 0); // Sum all centroids' y values
 
-    void process()
-    {
-        #ifdef DT_BUILD_DEV
-          // Clock for frame rate calculation
-          std::clock_t start( std::clock() );
-          double duration;
-        #endif
+  if (avg_x.size() > 0) {
+    X = (sum_x / float(avg_x.size())); // Average x coordinate
+    Y = (sum_y / float(avg_y.size())); // Average y coordinate
+  }
+  else {
+    // No contours found
+    W = 0;
+    X = -1.0;
+    Y = -1.0;
+    Z = -1.0;
 
-        // IMAGE PROCESSING HERE
-        cvtColor(frame, img_gray, CV_BGR2GRAY);              // Turn image to grayscale
+    #ifdef DT_BUILD_DEV
+      ROS_INFO("No contours detected.");
+    #endif
+  }
 
-        blur(frame, blurr, Size(blur_size, blur_size), Point(-1, -1)); // Blur image
+  // TODO: Publish OF velocities
 
-        Canny(blurr, dil, lowThreshold, lowThreshold*ratio, kernel_size ); // Find edges
-        dilate(dil, erod, Mat(), Point(-1, -1), 4, 1,1); // Attempt to get rid of noise
-        //erode(dil, dil, Mat(), Point(-1, -1), 4, 1, 1);
+  // Publish average point x, y,
+  // directon from bottom point to average point,
+  // number of found points
 
-        Mat mask = cv::Mat::zeros(erod.size(), erod.type()); // all 0
-        mask(Rect(mask_w, 0,mask.cols-2*mask_w,mask.rows)) = 1;
-        erod.copyTo(canny_output, mask);
+  if( X >= 0 and Y >= 0) {
+    X -= CAM_FRAME_OFFSET_X;
+    Y = CAM_FRAME_HEIGHT - Y - CAM_FRAME_OFFSET_Y; // Conv Y to bottom up
+    bot_X -= CAM_FRAME_OFFSET_X;
+    bot_Y = CAM_FRAME_HEIGHT - bot_Y - CAM_FRAME_OFFSET_Y;
+    Z = atan2(X-bot_X,Y-bot_Y);
+    W = avg_x.size();
+  }
 
-        findContours(canny_output, contours, hierarchy, RETR_TREE,CHAIN_APPROX_SIMPLE, Point(0, 0)); // Get contours
+  //msg.header.frame_id = "frames";
+  msg.header.stamp = ros::Time::now();
+  msg.quaternion.x = X / float(CAM_FRAME_WIDTH) * 2.0;  // Norm to half frame
+  msg.quaternion.y = Y / float(CAM_FRAME_HEIGHT) * 2.0;
+  msg.quaternion.z = Z;
+  msg.quaternion.w = W;
 
-
-        // GETTING CONTOUR CENTROIDS HERE
-
-        vector<Moments> mu; // Moments for calculating centroids
-        mu.reserve(contours.size());
-
-        for (size_t i = 0; i < contours.size(); i++) // Look through all contours
-        {
-
-            if (contourArea(contours[i]) > lower and contourArea(contours[i]) < upper)
-            {
-
-                mu.push_back(moments(contours[i], false));
-            }
-
-        } // Calculate moments
-
-        vector<Point2f> mc(mu.size());
-        vector<float> avg_x(mu.size());
-        vector<float> avg_y(mu.size());
-
-        for (size_t i = 0; i < mu.size(); i++) // Based on moments calculate centroids
-            {
-                avg_x[i] = float(mu[i].m10 / mu[i].m00);
-                avg_y[i] = float(mu[i].m01 / mu[i].m00);
-
-                #ifdef DT_BUILD_DEV
-                  mc[i] =
-                      Point2f(static_cast<float>(mu[i].m10 / mu[i].m00),
-                              static_cast<float>(mu[i].m01 / mu[i].m00)); // for visuals
-                #endif
-
-                if (i==0){
-                    bot_X = static_cast<float>(mu[i].m10 / mu[i].m00);
-                    bot_Y = static_cast<float>(mu[i].m01 / mu[i].m00);
-                }
-            }
-
-        // CALCULATING MIDPOINT OF ALL CENTROIDS HERE
-
-        sum_x = accumulate(avg_x.begin(), avg_x.end(), 0); // Sum all centroids x values
-
-        if (avg_x.size() == 0)
-        {
-            X = -1;
-            W = 0;
-            Z = -1;
-
-            #ifdef DT_BUILD_DEV
-              ROS_INFO("No contours detected.");
-            #endif
-        }
-        else
-        {
-            X = (sum_x / avg_x.size()); // Get average x coordinate.
-        }
-
-        sum_y = accumulate(avg_y.begin(), avg_y.end(), 0); // Sum all centroids y values
-
-        if (avg_y.size() == 0)
-        {
-            Y = -1;
-        }
-        else
-        {
-            Y = (sum_y / avg_y.size()); // Get average y coordinate.
-        }
-
-        vector<Point2f> XY(1);
-        XY[0] =
-            Point2f(static_cast<float>(X), static_cast<float>(Y)); // For visuals
+  point_pub.publish(msg);
 
 
+  // VISUALIZING EVERYTHING HERE
+  #ifdef DT_BUILD_DEV
+    canny_output = canny_output * 0.7;
 
-
-        // VISUALIZING EVERYTHING HERE
-        #ifdef DT_BUILD_DEV
-            canny_output = canny_output * 0.7;
-
-            for (size_t i = 1; i < mc.size(); i++) // Visualize the centroids
-            {
-                circle(canny_output, mc[i], 4, Scalar(100,100,100), 2, 8, 0);
-            }
-
-            if (mc.size() > 0) {
-              circle(canny_output, XY[0], 10, Scalar(180,180,180), -1, 8, 0);
-              circle(canny_output, mc[0], 8, Scalar(140,140,140), -1, 8, 0);
-            }
-
-            imshow("Contours", canny_output);
-            waitKey(1);
-        #endif
-
-
-
-        // PUBLISHING STUFF HERE
-
-        if( X >= 0 and Y >= 0) {
-            X += X_offset;
-            Y = (height - Y) + Y_offset; // (height-Y) makes the Y
-            // coordinate start from the bottom.
-            bot_X += X_offset;
-            bot_Y = (height - bot_Y) + Y_offset;
-            Z = atan2(X-bot_X,Y-bot_Y);
-            W = avg_x.size(); //1;
-        }
-
-        msg.header.frame_id = "frames";
-        msg.header.stamp = ros::Time::now();
-
-        msg.quaternion.x = X / double(width) * 2.0;
-        msg.quaternion.y = Y / double(height) * 2.0;
-        msg.quaternion.z = Z;
-        msg.quaternion.w = W;
-
-        point_pub.publish(msg);
-        // Publish point
-
-        /*
-        if (visualize){
-            if (W==1){
-                ROS_INFO("Msg Sent.");
-                ROS_INFO("Subscribers: %d", point_pub.getNumSubscribers());
-            }
-
-            // STOP CLOCK HERE, CALCULATE FPS
-            duration = (std::clock() - start) / (double)CLOCKS_PER_SEC; // Get FPS here
-            cout << "printf: " << 1 / duration << '\n';
-        }
-        */
+    // Draw centroid markers
+    for (size_t i = 1; i < mc.size(); i++) {
+        circle(canny_output, mc[i], 4, Scalar(100), 2, 8, 0);
     }
 
-    virtual void spin()
-    {
-        ros::Rate rate(20);
-        while (ros::ok())
-        {
-            if (image_flag)
-            {
-                process();
-                image_flag = false;
-            }
-
-            ros::spinOnce();
-            rate.sleep();
-        }
+    // Draw average and bottom markers
+    if (mc.size() > 0) {
+      Point2f XY(static_cast<float>(X), static_cast<float>(Y));
+      circle(canny_output, XY, 10, Scalar(180), -1, 8, 0);
+      circle(canny_output, mc[0], 8, Scalar(140), -1, 8, 0);
     }
+
+    imshow("Contours", canny_output);
+    waitKey(1);
+
+    /*
+      if (W==1){
+        ROS_INFO("Msg Sent.");
+        ROS_INFO("Subscribers: %d", point_pub.getNumSubscribers());
+      }
+    */
+
+    // Calculate and display fps
+    float fps = 1.0f / (ros::Time::now().toSec() - startTime); // (float) CLOCKS_PER_SEC; // Get FPS here
+    cout << "ros fps: " << fps << '\n';
+  #endif
+
+
+  // Copy data for next iteration
+  frame_gray.copyTo(frame_gray_old);
+  frame_old_time = frame_time;
+  of_old_points = of_new_points;
+  ++of_refresh_counter;
+}
+
+
+/* SETUP AND START */
+
+void Visuals::run() {
+  ros::Rate rate(CAM_LOOP_RATE);
+
+  // Wait for first frame to arrive
+  while(ros::ok() && !image_flag) {
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  // Init optical flow
+  cvtColor(frame, frame_gray_old, CV_BGR2GRAY);
+  frame_old_time = frame_time;
+  image_flag = false;
+
+  feature_detector = cv::FastFeatureDetector::create(50, true);
+  of_criteria = cvTermCriteria(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
+  of_win_size = cvSize(15,15);
+
+
+  // Create image mask
+  mask = cv::Mat::zeros(frame.size(), CV_8UC1);
+  mask( Rect( CAM_FRAME_MASK_WIDTH, 0,
+              mask.cols - 2 * CAM_FRAME_MASK_WIDTH,
+              mask.rows )
+  ) = Scalar(1);
+
+
+  // Main processing loop
+  while (ros::ok()) {
+    if (image_flag) {
+      process();
+      image_flag = false;
+    }
+
+    ros::spinOnce();
+    rate.sleep();
+  }
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "visualizer");
-
+    ros::init(argc, argv, "robotex_cam");
     ros::NodeHandle nh;
-    ros::NodeHandle nhPrivate("~");
-
-    visuals *node = 0;
+    Visuals node(nh);
 
     #ifdef DT_BUILD_DEV
       namedWindow("Contours", WINDOW_AUTOSIZE);
       startWindowThread();
     #endif
 
-    node = new visuals(nh, nhPrivate);
-
-    node->spin();
+    node.run();
     return 0;
 }
---
+++ cam/src/cam.hpp
@@ -0,0 +1,83 @@
+#ifndef DT_CAM_INCLUDED
+#define DT_CAM_INCLUDED
+
+#include "image_transport/image_transport.h"
+#include "geometry_msgs/QuaternionStamped.h"
+#include "opencv2/opencv.hpp"
+
+#define CAM_LOOP_RATE           15  // Hz
+
+#define CAM_FRAME_WIDTH        640
+#define CAM_FRAME_HEIGHT       480
+#define CAM_FRAME_OFFSET_X     320
+#define CAM_FRAME_OFFSET_Y     240
+#define CAM_FRAME_MASK_WIDTH   200
+
+#define CAM_CANNY_THR_LOW      121
+#define CAM_CANNY_THR_HIGH     (CAM_CANNY_THR_LOW * 1.5)
+#define CAM_CANNY_KERNEL_SIZE    3
+#define CAM_BLUR_SIZE            3
+#define CAM_CONTOUR_LIM_LOW   1000
+#define CAM_CONTOUR_LIM_HIGH 10000
+
+#define CAM_OF_REFRESH_INTERVAL  5  // frames
+#define CAM_OF_MAX_LEVELS        2  // OF pyramid levels
+//#define CAM_OF_MAX_POINTS     20
+//#define CAM_OF_QUALITY       0.3
+//#define CAM_OF_MIN_DIST       10
+//#define CAM_OF_BLOCK_SIZE      7
+
+
+class Visuals {
+public:
+  Visuals(ros::NodeHandle &nh_);
+  ~Visuals();
+
+  void imgcb(const sensor_msgs::ImageConstPtr &msg);
+  void process();
+  void run();
+
+protected:
+  image_transport::ImageTransport it_;
+  geometry_msgs::QuaternionStamped msg;
+  image_transport::Subscriber sub;
+  ros::Publisher point_pub;
+
+private:
+  bool image_flag;
+
+  // Frame storage
+  cv::Mat frame, frame_gray, frame_gray_old;
+  float frame_time, frame_old_time;
+
+  // Optical flow
+  std::vector<cv::Point2f> of_old_points;
+  std::vector<cv::Point2f> of_new_points;
+  std::vector<uchar> of_status; // TODO: maybe init to CAM_OF_MAX_POINTS
+  cv::Mat of_err;
+  int of_refresh_counter;
+  cv::Ptr<cv::FastFeatureDetector> feature_detector;
+  CvTermCriteria of_criteria;
+  CvSize of_win_size;
+  std::vector<cv::KeyPoint> of_keypoints;
+  std::vector<cv::Point2f> of_velocities;
+  cv::Point2f of_mean_velocity;
+
+  // Image processing storage
+  cv::Mat img_gray, img_bw, img_final;
+  cv::Mat blurr, erod, dil;
+  cv::Mat mask, masked;
+  cv::Mat canny_output;
+
+  std::vector<std::vector<cv::Point> > contours;
+  std::vector<cv::Moments> mu;
+  std::vector<cv::Vec4i> hierarchy;
+  std::vector<float> avg_x, avg_y;
+  float sum_x, sum_y, X, Y, bot_X, bot_Y, Z;
+  int W;
+  #ifdef DT_BUILD_DEV
+    std::vector<cv::Point2f> mc;
+  #endif
+};
+
+#endif
